67fb975f34d13d63befdc04de1d5ec125a76346b
tami093 <crisoliveira.93@gmail.com>
2016-03-09 19:25:08 +0000

    classe carro

---
+++ Carro.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * Classe Carro
+ *
+ **/
+class Carro
+{
+    private $_cor;
+
+    public function getCor()
+    {
+        return $this->_cor;
+    }
+
+    public function setCor($cor)
+    {
+        $this->_cor = $cor;
+    }
+
+}
+?>
